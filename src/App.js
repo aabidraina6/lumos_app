8c855bc14ceae092d89fbb470494f9d369f25e91
aabidraina6 <aabidraina5@gmail.com>
2023-05-24 06:55:09 +0530

    styled-components removed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-
 function App() {
+
+
   return (
     <div className="App">
+      <Buttonhead onClick={()  =>{console.log("here is the styled button")}}>button</Buttonhead>
      <p>this is a para</p>
     </div>
   );
